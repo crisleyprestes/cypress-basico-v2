5ba56fe406000c60165ec879ab46ec7b996bd2b2
Crisley Linhares <prestes40@gmail.com>
2023-02-21 16:48:40 -0400

    Aula 15 - Validando mensagem de erro.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -43,7 +43,7 @@ describe('Central de Atendimento ao Cliente TAT', function(){
         cy.get('.error').should('be.visible')
     })
 
-    it.only('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
         cy.get('#firstName')
             .type('Crisley')
             .should('have.value', 'Crisley')
@@ -64,5 +64,10 @@ describe('Central de Atendimento ao Cliente TAT', function(){
             .should('have.value', '987654321')
             .clear()
             .should('have.value', '')
+    })
+
+    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
     })
 })
