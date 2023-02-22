995fba09f6c8c68c186851f362f60a890ca20af0
Crisley Linhares <prestes40@gmail.com>
2023-02-22 18:24:02 -0400

    Aula 33 - Testando se link abre em outra aba.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -145,4 +145,8 @@ describe('Central de Atendimento ao Cliente TAT', function(){
         })
     })
 
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy > a').should('have.attr', 'target', '_blank')
+    })
+
 })
