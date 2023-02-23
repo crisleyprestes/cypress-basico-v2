3b51be39ebc978319b5c07fcec122bf16f0a5ec9
Crisley Linhares <prestes40@gmail.com>
2023-02-22 22:26:17 -0400

    Aula 56 - Encontre o gato

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -210,4 +210,15 @@ describe('Central de Atendimento ao Cliente TAT', function(){
                 expect(body).to.include('CAC TAT')
             })
     })
+
+    it.only('encontra o gato escondido', function(){
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
+
+        cy.get('#title')
+            .invoke('text', 'CAT TAT')
+        cy.get('#subtitle')
+            .invoke('text', 'Eu amo gatos!')        
+    })
 })
