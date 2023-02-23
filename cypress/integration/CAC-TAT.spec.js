6a8a7d54f327e4b7ff257b009b5de90d7695876d
Crisley Linhares <prestes40@gmail.com>
2023-02-22 22:17:25 -0400

    Aula 54 - Comando cy.request

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -193,11 +193,21 @@ describe('Central de Atendimento ao Cliente TAT', function(){
             .should('not.be.visible')    
     })
 
-    it.only('preenche a área de texto usando o comando invoke', function(){
+    it('preenche a área de texto usando o comando invoke', function(){
         const longText = Cypress._.repeat('0123456789', 20)
 
         cy.get('#open-text-area')
             .invoke('val', longText)
             .should('have.value', longText)
+    })
+
+    it('faz uma requisição HTTP', function(){
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response){
+                const{status, statusText, body} = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
     })
 })
